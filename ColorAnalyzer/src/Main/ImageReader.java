48b4e5823177b1ddccd8fac5ba8006f39f949272
FineiasAntonio <138174957+FineiasAntonio@users.noreply.github.com>
2023-10-28 19:13:56 -0300

    Add files via upload

--- ColorAnalyzer/src/Main/ImageReader.java
+++ ColorAnalyzer/src/Main/ImageReader.java
@@ -11,6 +11,7 @@ import java.util.function.Function;
 import javax.imageio.ImageIO;
 
 public class ImageReader {
+
 public static List<Cor> lerPixels(File image) throws IOException{
     HashMap<String, Integer> ColorMap = new HashMap<>();
     List<Cor> coresList = new LinkedList<>();
@@ -19,10 +20,12 @@ public static List<Cor> lerPixels(File image) throws IOException{
             final long TAM = bufferedImage.getWidth()*bufferedImage.getHeight();
             Function<String, Integer> verificar = (key -> !ColorMap.containsKey(key)?1:ColorMap.get(key) + 1);
 
-
             for(int i = 1; i < bufferedImage.getWidth(); i++){
                 for(int j = 1; j < bufferedImage.getHeight(); j++){
+                    
                     Cor corInput = new Cor(Cor.toHex(bufferedImage.getRGB(i, j)));
+                    
+
 
                     ColorMap.put(corInput.getCódigoCor(), verificar.apply(corInput.getCódigoCor()));
                 }
@@ -37,5 +40,13 @@ public static List<Cor> lerPixels(File image) throws IOException{
         bufferedImage.flush();
 
         return coresList;
+    }
+
+    public static String ValueAproximation(String color) {
+        String red = color.substring(0, 1);
+        String green = color.substring(2, 3);
+        String blue = color.substring(4,5);
+
+        return "O JOGO FIlhO DA PUTA";
     }
 }
